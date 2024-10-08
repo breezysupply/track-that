4ff73707a12307da6d95c67a243eb25c7fd99b31
Brett Pierce <bapierce88@gmail.com>
2024-10-07 21:40:04 -0400

    fixed tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,13 +14,18 @@ const config: Config = {
         sans: ['Inter', ...fontFamily.sans],
       },
       animation: {
-        flash: 'flash 0.5s',
+        flash: 'flash 1s infinite',
       },
       keyframes: {
         flash: {
-          '0%, 100%': { opacity: 0 },
-          '50%': { opacity: 0.5 },
+          '0%, 100%': { opacity: '0' },
+          '50%': { opacity: '0.5' },
         },
+      },
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
   },
