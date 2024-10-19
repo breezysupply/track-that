ac66812878ff901047e47c3252e436c0c9be7b3a
Brett Pierce <bapierce88@gmail.com>
2024-10-18 22:27:17 -0400

    more linting issues

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,30 +1,27 @@
 import type { Config } from 'tailwindcss'
-import { fontFamily } from 'tailwindcss/defaultTheme'
 
 const config: Config = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  darkMode: 'class', // Add this line
   theme: {
     extend: {
-      fontFamily: {
-        sans: ['Inter', ...fontFamily.sans],
-      },
-      animation: {
-        flash: 'flash 0.5s',
-      },
-      keyframes: {
-        flash: {
-          '0%, 100%': { opacity: 0 },
-          '50%': { opacity: 0.5 },
-        },
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
   },
   plugins: [],
+  keyframes: {
+    flash: {
+      '0%, 100%': { opacity: '0' },
+      '50%': { opacity: '0.5' },
+    },
+  },
 }
 
 export default config
